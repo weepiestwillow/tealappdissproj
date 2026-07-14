a48446f247867e8ae436da7062dc8ddd49d21122
Willow Smith <exrobite@gmail.com>
2026-07-14 17:51:22 +0100

    book excercises

--- Dissertation Project 2/multiplication sliders.R
+++ Dissertation Project 2/multiplication sliders.R
@@ -2,12 +2,29 @@ ui <- fluidPage(
   sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
   sliderInput("y", label = "and y is", min = 1, max = 50, value = 30),
   "then x times y is",
-  textOutput("product")
+  textOutput("product"),
+  "and x times y plus five is",
+  textOutput("productplusfive"),
+  "and x times y plus ten is",
+  textOutput("productplusten")
 )
 
 server <- function(input, output, session) {
-  output$product <- renderText({ 
+  
+  multiplied <- reactive({
     input$x * input$y
+  })
+  
+  output$product <- renderText({ 
+    multiplied()
+  })
+  
+  output$productplusfive <- renderText({
+    multiplied() + 5
+  })
+  
+  output$productplusten <- renderText({
+    multiplied() + 10
   })
 }
 
