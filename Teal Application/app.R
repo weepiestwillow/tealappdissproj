7d1c78b3697ee37970db4088c4c20a7d9f98122b
Willow Smith <exrobite@gmail.com>
2026-07-20 18:32:27 +0100

    Implemented kaplan meier module

--- Teal Application/app.R
+++ Teal Application/app.R
@@ -1,12 +1,24 @@
 library(teal)
 library(teal.modules.general)
+library(random.cdisc.data)
+library(teal.modules.clinical)
+
+set.seed(314)
+ADSL <- radsl()
+ADAE <- radae(ADSL)
+ADTTE <- radtte(ADSL)
 
 data <- teal_data(
-  iris2 = iris,
-  mtcars2 = mtcars,
+  ADSL = ADSL,
+  ADAE = ADAE,
+  ADTTE = ADTTE,
+  join_keys = default_cdisc_join_keys,
   code = "
-  iris2 <- iris
-  mtcars2 <- mtcars"
+  set.seed(314)
+  ADSL <- radsl()
+  ADAE <- radae(ADSL)
+  ADTTE <- radtte(ADSL)
+  "
 )
 
 data <- verify(data)
@@ -14,11 +26,41 @@ data <- verify(data)
 app <- init(
   data = data,
   modules = modules(
-    example_module(),
-    tm_data_table()
+    tm_variable_browser(
+      label = "Data Overview"
+    ),
+    tm_g_km(
+      label = "Kaplan Meier plot",
+      dataname = "ADTTE",
+      arm_var = choices_selected(
+        choices = variable_choices(data[["ADSL"]], c("ARM", "ARMCD")),
+        selected = "ARM"
+      ),
+
+      strata_var = choices_selected(
+        choices = variable_choices(data[["ADSL"]], c("SEX", "AGE", "RACE")),
+        selected = NULL 
+      ),
+      paramcd = choices_selected(
+        choices = value_choices(data[["ADTTE"]], "PARAMCD"),
+        selected = "OS"
+      ),
+      aval_var = choices_selected(
+        choices = variable_choices(data[["ADTTE"]], "AVAL"),
+        selected = "AVAL"
+      ),
+      cnsr_var = choices_selected(
+        choices = variable_choices(data[["ADTTE"]], "CNSR"),
+        selected = "CNSR"
+      ),
+      facet_var = choices_selected(
+        choices = variable_choices(data[["ADSL"]], c("SEX", "RACE")),
+        selected = "SEX"
+      )
+    )
   )
 )
 
-#hello world
+
 
 shinyApp(app$ui, app$server)
